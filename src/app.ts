df11539ffea54c49e12f70853651ac03a7b560e4
NguyenAnhThang <thangna@ftech.ai>
2024-03-06 23:27:01 +0700

    update

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req: any, res: { send: (arg0: string) => any }) => {
+    res.send("Express on Vercel");
+});
+
+app.get("/hello", (req: any, res: { send: (arg0: string) => any }) => {
+    res.send("Xin Chào");
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
